0fc6189033f18e259e77a2efc0477861d009bd07
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2025-01-12 13:00:02 -0800

    Create logic for dark mode button and setting dark mode based on OS preferences

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
 	eventListeners();
+
+	darkMode();
 });
+
+function darkMode() {
+	const isDarkMode = localStorage.getItem('darkMode') === 'true';
+	const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+	if ( isDarkMode || prefersDarkMode.matches ) {
+		document.body.classList.add('dark-mode');
+	}
+
+	prefersDarkMode.addEventListener('change', () => {
+		document.body.classList.toggle('dark-mode');
+	});
+
+	const darkModeButton = document.querySelector('.dark-mode-button');
+	darkModeButton.addEventListener('click', () => {
+		document.body.classList.toggle('dark-mode');
+
+		const darkModeState = document.body.classList.contains('dark-mode');
+		localStorage.setItem('darkMode', darkModeState);
+	});
+ }
 
 function eventListeners() {
 	const mobileMenu = document.querySelector('.mobile-menu');
