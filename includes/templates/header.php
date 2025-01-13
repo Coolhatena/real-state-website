9e37befd9bb69c5f02a9cb5e70923b78fc453e38
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2025-01-13 13:36:09 -0800

    Create separate file for header

---
+++ includes/templates/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bienes Raices</title>
+	<link rel="stylesheet" href="build/css/app.css">
+
+</head>
+<body>
+
+	<header class="header <?php echo isset( $mainPage ) ? 'main' : '' ?>">
+		<div class="container header-content">
+			<div class="bar">
+				<a href="/"><img src="build/img/logo.svg" alt="Logotipo de Bienes Raices"></a>
+
+				<div class="mobile-menu">
+					<img src="build/img/barras.svg" alt="Icono Menu responsive">
+				</div>
+
+				<div class="header-right">
+					<nav class="navigation">
+						<a href="us.php">Nosotros</a>
+						<a href="ads.php">Anuncios</a>
+						<a href="blog.php">Blog</a>
+						<a href="contact.php">Contacto</a>
+					</nav>
+					<img src="build/img/dark-mode.svg" alt="Icono modo oscuro" class="dark-mode-button">
+				</div>
+				
+			</div>
+			<?php echo isset( $mainPage ) ? '<h1>Venta de Casas y Departamentos Exclusivos de Lujo</h1>' : '' ?>
+		</div> <!-- End bar -->
+	</header>
