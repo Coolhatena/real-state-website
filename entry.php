52e528150b309e9e4ca602e746d66b307689caf4
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2025-01-13 13:43:54 -0800

    Import footer

--- entry.php
+++ entry.php
@@ -17,19 +17,7 @@
 		</div>
 	</main>
 
-	<footer class="footer section">
-		<div class="container footer-container">
-			<div class="navigation">
-				<a href="us.php">Nosotros</a>
-				<a href="ads.php">Anuncios</a>
-				<a href="blog.php">Blog</a>
-				<a href="contact.php">Contacto</a>
-			</div>
-		</div>
-
-		<p class="copyright">Todos los derechos Reservados 2025 &copy;</p>
-		<p class="github">Más de mis proyectos aquí: <a href="https://github.com/Coolhatena?tab=overview&from=2025-01-01&to=2025-01-06"> Github/Coolhatena</a> ;)</p>
-	</footer>
+	<?php include 'includes/templates/footer.php' ?>
 	
 	<script src="build/js/bundle.min.js"></script>
 </body>
