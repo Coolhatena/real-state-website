1204c70d70a8cf4c04f7dbaefcd3dd4073e5e3a8
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2025-01-15 15:24:02 -0800

    Adapt header to be used on any project folder

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Bienes Raices</title>
-	<link rel="stylesheet" href="build/css/app.css">
+	<link rel="stylesheet" href="/build/css/app.css">
 
 </head>
 <body>
@@ -12,10 +12,10 @@
 	<header class="header <?php echo $isAlt ? 'main' : '' ?>">
 		<div class="container header-content">
 			<div class="bar">
-				<a href="/"><img src="build/img/logo.svg" alt="Logotipo de Bienes Raices"></a>
+				<a href="/"><img src="/build/img/logo.svg" alt="Logotipo de Bienes Raices"></a>
 
 				<div class="mobile-menu">
-					<img src="build/img/barras.svg" alt="Icono Menu responsive">
+					<img src="/build/img/barras.svg" alt="Icono Menu responsive">
 				</div>
 
 				<div class="header-right">
@@ -25,7 +25,7 @@
 						<a href="blog.php">Blog</a>
 						<a href="contact.php">Contacto</a>
 					</nav>
-					<img src="build/img/dark-mode.svg" alt="Icono modo oscuro" class="dark-mode-button">
+					<img src="/build/img/dark-mode.svg" alt="Icono modo oscuro" class="dark-mode-button">
 				</div>
 				
 			</div>
