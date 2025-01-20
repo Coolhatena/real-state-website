bf692c6a454a8a7521c1e3f17c36e0751d730e2c
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2025-01-20 10:52:02 -0800

    Insert form values into the database

--- admin/properties/create.php
+++ admin/properties/create.php
@@ -4,12 +4,26 @@
 	$db = connectDB();
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		echo "<pre>";
-		var_dump($_POST);
-		echo "</pre>";
+		// echo "<pre>";
+		// var_dump($_POST);
+		// echo "</pre>";
 
 		$title = $_POST["title"];
 		$price = $_POST["price"];
+		$description = $_POST["description"];
+		$rooms = $_POST["rooms"];
+		$wc = $_POST["wc"];
+		$parkings = $_POST["parkings"];
+		$seller_id = $_POST["seller_id"];
+
+		// Insert
+		$query = "INSERT INTO properties (title, price, description, rooms, wc, parkings, seller_id) VALUES ( '$title', '$price', '$description', '$rooms', '$wc', '$parkings', '$seller_id');";
+		// echo $query;
+		
+		$result = mysqli_query($db, $query);
+		if ($result) {
+			echo "Insertado correctamente";
+		}
 	}
 
 	require '../../includes/functions.php';
@@ -32,7 +46,7 @@
 				<input type="number" name="price" id="price" placeholder="Precio Propiedad">
 
 				<label for="imagen">Imagen:</label>
-				<input type="file" name="image" id="image" accept="image/jpeg, image/png">
+				<input type="file" id="image" accept="image/jpeg, image/png">
 
 				<label for="description">Descripción</label>
 				<textarea name="description" id="description"></textarea>
@@ -48,13 +62,13 @@
 				<input type="number" name="wc" id="wc" placeholder="Ej: 3" min="1">	
 
 				<label for="parking">Estacionamientos:</label>
-				<input type="number" name="parking" id="parking" placeholder="Ej: 3" min="1">	
+				<input type="number" name="parkings" id="parkings" placeholder="Ej: 3" min="1">	
 			</fieldset>
 
 			<fieldset>
 				<legend>Información del Vendedor</legend>
 
-				<select name="seller" id="seller">
+				<select name="seller_id" id="seller_id">
 					<option value="1">Eduardo</option>
 					<option value="2">Evelyn</option>
 				</select>
