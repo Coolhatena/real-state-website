2aaf4e596ce84bde856696eca62f9fc19a509ef3
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2025-01-26 11:36:31 -0800

    Add base for properties display

--- admin/index.php
+++ admin/index.php
@@ -16,6 +16,30 @@
 
 
 		<a href="/admin/properties/create.php" class="button button-green">Agregar propiedad</a>
+		
+		<table class="properties">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Titulo</th>
+					<th>Imagen</th>
+					<th>Precio</th>
+					<th>Acciones</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1</td>
+					<td>Casa en la playa</td>
+					<td> <img src="/images/649383509ebbeab61b1a48c839ba1bd8.jpg" alt="Imagen propiedad" class="table-image"> </td>
+					<td>$1200000</td>
+					<td>
+						<a href="#" class="button-yellow-block">Actualizar</a>
+						<a href="#" class="button-red-block">Eliminar</a>
+					</td>
+				</tr>
+			</tbody>
+		</table>
 	</main>
 
 	<?php includeTemplate('footer'); ?>
