08c926daa540d6bbbd4be71804c97840c9dcbdbf
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2025-02-09 19:37:07 -0800

    Create user using php

---
+++ user.php
@@ -0,0 +1,18 @@
+<?php
+	exit;
+	// Import connection
+	require 'includes/config/database.php';
+	$db = connectDB();
+
+	// Create email and password
+	$email = 'correo@correo.com';
+	$password = '123456';
+	$password_hashed = password_hash($password, PASSWORD_DEFAULT);
+	
+	//Query to create user
+	$query = "INSERT INTO users (email, password) VALUES ('$email', '$password_hashed');";
+
+	echo $query;
+
+	// Add user to db
+	// mysqli_query($db, $query);
