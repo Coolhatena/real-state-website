2209a62bd11edb11a32afafac0a80a9234143d48
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2025-02-14 18:00:19 -0800

    Create function to check login

--- includes/functions.php
+++ includes/functions.php
@@ -5,3 +5,8 @@ require 'app.php';
 function includeTemplate( string $name, bool $isAlt = false ) {
 	include TEMPLATES_URL . $name . '.php';
 }
+
+function isAuth() : bool {
+	session_start();	
+	return !!$_SESSION['login'];
+}
